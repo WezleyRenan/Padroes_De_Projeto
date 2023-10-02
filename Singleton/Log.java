a199c58ecf5998ed6b8fe5dc8be22f198a1d9e22
Qen <118078373+WezleyRenan@users.noreply.github.com>
2023-10-02 22:38:19 +0000

    melhoria das classes de Singleton

--- Singleton/Log.java
+++ Singleton/Log.java
@@ -1,23 +1,26 @@
-public class Singleton {
+package Singleton;
+
+public class Log {
     // 1. Variável estática privada para armazenar a instância única
-    private static Singleton instance;
+    private static Log instance;
+    int numeroOperaçao = 0;
 
     // 2. Construtor privado para evitar a criação de instâncias diretas
-    private Singleton() {
+    private Log() {
         // Qualquer lógica de inicialização desejada aqui
-        System.out.println("Singleton foi criado.");
+        System.out.println("iniciado uma operação Numero: ");
     }
 
     // 3. Método público para obter a instância única (com inicialização preguiçosa)
-    public static Singleton getInstance() {
+    public static Log getInstance() {
         if (instance == null) {
-            instance = new Singleton();
+            instance = new Log();
         }
         return instance;
     }
 
     // Adicione métodos e propriedades adicionais conforme necessário
-    public void doSomething() {
-        System.out.println("Singleton está fazendo algo.");
+    public void retornar(String operação) {
+        System.out.println("Foi feita uma operação de: " + operação + "// da conta de numero: " + ++numeroOperaçao);
     }
 }
--- Singleton/Main.java
+++ Singleton/Main.java
@@ -1,15 +1,18 @@
+package Singleton;
+
 public class Main {
     public static void main(String[] args) {
         // 4. Obtendo a instância do Singleton
-        Singleton singleton1 = Singleton.getInstance();
-        Singleton singleton2 = Singleton.getInstance();
+        Log singleton1 = Log.getInstance();
+        Log singleton2 = Log.getInstance();
+        Log singleton3 = Log.getInstance();
+        Log singleton4 = Log.getInstance();
 
-        // 5. Verificando se as duas variáveis apontam para a mesma instância
-        if (singleton1 == singleton2) {
-            System.out.println("As duas variáveis apontam para a mesma instância do Singleton.");
-        }
-
-        // 6. Usando métodos da instância Singleton
-        singleton1.doSomething();
+        
+        // 6. Usando métodos #1 da instância Singleton
+        singleton1.retornar("Adição");
+        singleton2.retornar("Subtração");
+        singleton3.retornar("Multiplicação");
+        singleton4.retornar("divisão");
     }
 }
